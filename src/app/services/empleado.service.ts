b25ba90386d0de289bd1d936e4169b99439cff33
Laureano Ezequiel Alvarez <137580645+Ezeki-23@users.noreply.github.com>
2024-05-06 17:36:30 -0300

    Update empleado.service.ts

--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -7,7 +7,7 @@ import { Empleado } from '../interfaces/empleado';
   providedIn: 'root'
 })
 export class EmpleadoService {
-  private myAppUrl: string = 'http://ezekiel024-001-site1.btempurl.com/';
+  private myAppUrl: string = 'http://gestionapp.somee.com/';
   private myApiUrl: string = 'api/Empleado/';
 
   constructor(private http: HttpClient) { }
